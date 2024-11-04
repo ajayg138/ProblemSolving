e40cc076b9e42e1b070cb63be7d0a4e70f852a5c
ajayg138 <ajaygole728@gmail.com>
2024-11-04 20:32:36 +0530

    LinkedListEx

---
+++ LinkedListEx.java
@@ -0,0 +1,30 @@
+public class LinkedListEx{
+
+
+    public static class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+    public static void printList(Node n){
+        while(n != null){
+            System.out.print(n.data+" ");
+            n=n.next;
+        }
+    }
+
+    public static void main(String[] arggs){
+        Node head=new Node(1);
+        Node second=new Node(2);
+        Node third=new Node(3);
+
+        head.next=second;
+        second.next=third;
+        printList(head);
+    }
+}
