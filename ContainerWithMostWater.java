22e3e4e75ff87d0f178529c524f22056166ce2b1
ajayg138 <ajaygole728@gmail.com>
2024-11-05 23:29:32 +0530

    ContainerWithMostWater changes...

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -4,28 +4,61 @@ public class ContainerWithMostWater{
 
 
 //Brute Force Appraoch.................
-    public static int maxArea(int[] arr){
-        int maxiArea=0;
+    // public static int maxArea(int[] arr){
+    //     int maxiArea=0;
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                int height=Math.min(arr[i],arr[j]);
-                int width=j-i;
-                int area=height * width;
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=i+1;j<arr.length;j++){
+    //             int height=Math.min(arr[i],arr[j]);
+    //             int width=j-i;
+    //             int area=height * width;
 
-                if(area>maxiArea){
-                    maxiArea=area;
-                }
-            }
+    //             if(area>maxiArea){
+    //                 maxiArea=area;
+    //             }
+    //         }
+    //     }
+    //     return maxiArea;
+
+
+//Optimal Approach
+
+public static int maxArea(int[] arr){
+    int left=0;
+    int right=arr.length-1;
+    int max=0;
+
+
+    while(left<right){
+        int left_height=arr[left];
+        int right_height=arr[right];
+
+        int min_len=Math.min(left_height,right_height);
+        int breadth=right-left;
+
+        max = Math.max(max,min_len*breadth);
+
+        if(left_height<right_height){
+            left++;
+        }else{
+            right--;
         }
-        return maxiArea;
     }
+    return max;
+}
 
-    public static void main(String[] args){
-        int[] arr={1,8,6,2,5,4,8,3,7};
 
+ public static void main(String[] args){
+        // int[] arr={1,8,6,2,5,4,8,3,7};
+        int arr[] ={1,1};
         int res=maxArea(arr);
         System.out.print("Maximum Water in array: "+res);
 
     }
+
 }
+
+
+
+
+   
