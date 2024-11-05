19ce4fbe61094ecf454085dd5e50d33ccb24904a
ajayg138 <ajaygole728@gmail.com>
2024-11-05 22:36:20 +0530

    BinaryTreePaths changes...

--- BinaryTreePaths.java
+++ BinaryTreePaths.java
@@ -1,6 +1,25 @@
-public class BinaryTreePaths{
+class binaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root != null) {
+            findPaths(root, "", paths);
+        }
+        return paths;
+    }
 
-    public static void main(String[] args){
+    private void findPaths(TreeNode node, String path, List<String> paths) {
+        path += node.val;
         
+        if (node.left == null && node.right == null) {
+            paths.add(path); 
+        } else {
+            path += "->";
+            if (node.left != null) {
+                findPaths(node.left, path, paths);
+            }
+            if (node.right != null) {
+                findPaths(node.right, path, paths);
+            }
+        }
     }
 }
