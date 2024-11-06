99f4d99bc5692b14d075ea6b59bd80bc137bc34a
ajayg138 <ajaygole728@gmail.com>
2024-11-06 23:42:06 +0530

    ArraySort

--- ArraySort.java
+++ ArraySort.java
@@ -5,11 +5,14 @@ public class ArraySort{
     public static void sorting(int[] arr){
         Arrays.sort(arr);
 
-        System.out.print("Sorted Array is: "+arr);
+        for(int a:arr){
+            System.out.print(a+" ");
+        }
     }
 
     public static void main(String[] args){
         int[] arr={-4,-6,-8,-9,2,4,5,204,0};
+        sorting(arr);
 
     }
 }
