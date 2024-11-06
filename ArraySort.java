146d35f4ff1742346d6e5878a138d868ecd007ae
ajayg138 <ajaygole728@gmail.com>
2024-11-06 23:38:28 +0530

    ArraySort

---
+++ ArraySort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArraySort{
+
+    public static void sorting(int[] arr){
+        Arrays.sort(arr);
+
+        System.out.print("Sorted Array is: "+arr);
+    }
+
+    public static void main(String[] args){
+        int[] arr={-4,-6,-8,-9,2,4,5,204,0};
+
+    }
+}
