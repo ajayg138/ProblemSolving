f9db016a378822216b4e2e4548ec95931296252e
ajayg138 <ajaygole728@gmail.com>
2024-11-06 22:57:35 +0530

    TwoSum changes..

--- TwoSum.java
+++ TwoSum.java
@@ -2,17 +2,17 @@ public class TwoSum{
 
 
 //Brute Force Appraoch
-    public static int[] twosum(int[] arr,int target){
+    // public static int[] twosum(int[] arr,int target){
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    return new int[] {i,j};
-                }
-            }
-        }
-        return null;
-    }
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[i]+arr[j]==target){
+    //                 return new int[] {i,j};
+    //             }
+    //         }
+    //     }
+    //     return null;
+    // }
 
     public static void main(String[] args){
         // int[] arr={2,7,11,15};
