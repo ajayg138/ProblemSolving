7eecffccb308727100857a267f868435ace7b52e
ajayg138 <ajaygole728@gmail.com>
2024-11-07 15:39:56 +0530

    BalanceBracket..

--- BalanceBracket.java
+++ BalanceBracket.java
@@ -2,39 +2,44 @@ import java.util.*;
 
 public class BalanceBracket{
 
-    public static boolean balanceBrac(String str){
+    //Valid Parenthesis.......
+    // public static boolean balanceBrac(String str){
 
-        Stack<Character> st=new Stack<>();
+    //     Stack<Character> st=new Stack<>();
 
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
+    //     for(int i=0;i<str.length();i++){
+    //         char ch=str.charAt(i);
 
-            if(isOpening(ch)){
-                st.push(ch);
-            }else{
-                if(st.isEmpty()){
-                    return false;
-                }else if(!isMatching(st.peek(),ch)){
-                    return false;
-                }else{
-                    st.pop();
-                }
-            }
-        }
+    //         if(isOpening(ch)){
+    //             st.push(ch);
+    //         }else{
+    //             if(st.isEmpty()){
+    //                 return false;
+    //             }else if(!isMatching(st.peek(),ch)){
+    //                 return false;
+    //             }else{
+    //                 st.pop();
+    //             }
+    //         }
+    //     }
 
-        return st.isEmpty();
-    }
+    //     return st.isEmpty();
+    // }
 
-    public static boolean isOpening(char ch){
-        return ch=='(' || ch=='{' || ch=='[';
-    }
+    // public static boolean isOpening(char ch){
+    //     return ch=='(' || ch=='{' || ch=='[';
+    // }
 
-    public static boolean isMatching(char a,char b){
-        return (a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']');
-    }
+    // public static boolean isMatching(char a,char b){
+    //     return (a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']');
+    // }
+
+
+
+    //Balance Bracket....
 
     public static void main(String[] args){
-        String str="[(){()()}]";
+        String str="[(a+b)+{(c+d)*(e/f)}]";
         boolean res=balanceBrac(str);
         System.out.print(res);
 
