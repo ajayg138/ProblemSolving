19ef69199f5b0ada8b693fecde5f6783e034a7fe
ajayg138 <ajaygole728@gmail.com>
2024-11-07 14:33:32 +0530

    StackPrac

---
+++ StackPrac.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class StackPrac{
+
+
+    public static void main(String[] args){
+        Stack<Integer> st=new Stack<>();
+
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        System.out.print(st);
+    }
+}
