c94bedb1251855fb48dfb9571b6eaccb6dc0f475
ajayg138 <ajaygole728@gmail.com>
2024-11-07 15:07:23 +0530

    DuplicatessBracket

--- DuplicatesBracket.java
+++ DuplicatesBracket.java
@@ -1,29 +1,42 @@
 import java.util.*;
 
 public class DuplicatesBracket{
-    
-   public static void main(String[] args){
-    String str="((a+b)+((c+d)))";
 
-    Stack<Character> st=new Stack<>();
+    public static boolean CheackDup(String str){
 
-    for(int i=0;i<str.length();i++){
-        char ch=str.charAt(i);
-        if(ch==')'){
-            if(st.peek() == '('){
-                System.out.print(true);
-                return;
-            }else{
-                while(st.peek() != '('){
-                    st.pop();
-                }
-                st.pop();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+
+            if(ch==')'){
+
             }
         }else{
             st.push(ch);
         }
     }
-    System.out.print(false);
+    
+   public static void main(String[] args){
+    String str="((a+b)+((c+d)))";
+
+    // Stack<Character> st=new Stack<>();
+
+    // for(int i=0;i<str.length();i++){
+    //     char ch=str.charAt(i);
+    //     if(ch==')'){
+    //         if(st.peek() == '('){
+    //             System.out.print(true); //have duplicacy..
+    //             return;
+    //         }else{
+    //             while(st.peek() != '('){
+    //                 st.pop();
+    //             }
+    //             st.pop();
+    //         }
+    //     }else{
+    //         st.push(ch);
+    //     }
+    // }
+    // System.out.print(false);
 
 
     }
