51ea58563dd06c26d30a1d65de0219f5e26a0722
ajayg138 <ajaygole728@gmail.com>
2024-11-07 17:42:21 +0530

    NextGreaterElementOnRight changes...

--- NextGreaterElementOnRight.java
+++ NextGreaterElementOnRight.java
@@ -1,27 +1,57 @@
 import java.util.*;
 
-//Brute force O(n^2)
+
 public class NextGreaterElementOnRight{
-    public static void Next(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            boolean found=false;
-            for(int j=1+i;j<arr.length;j++){
-                if(arr[j]>arr[i]){
-                    System.out.println(arr[i]+"-->"+arr[j]);
-                    found=true;
-                    break;
-                }
-            }
-                if(!found){
-                    System.out.println(arr[i]+"-->"+-1);
-                }
+
+    // //Brute force O(n^2)
+//     public static void Next(int[] arr){
+//         for(int i=0;i<arr.length;i++){
+//             boolean found=false;
+//             for(int j=1+i;j<arr.length;j++){
+//                 if(arr[j]>arr[i]){
+//                     System.out.println(arr[i]+"-->"+arr[j]);
+//                     found=true;
+//                     break;
+//                 }
+//             }
+//                 if(!found){
+//                     System.out.println(arr[i]+"-->"+-1);
+//                 }
+//         }
+//     }
+
+
+//optimal O(n)...using Stack
+
+public static int[] solve(int[] arr){
+    int[] nge = new int[arr.length];
+
+    Stack<Integer> st=new Stack<>();
+
+    st.push(arr[arr.length-1]);
+    nge[arr.length-1]=-1;
+
+    for(int i=arr.length-2;i>=0;i--){
+        while(st.size() > 0 && arr[i]>=st.peek()){
+            st.pop();
         }
+
+        if(st.size() == 0){
+            nge[i]=-1;
+        }else{
+            nge[i]=st.peek();
+        }
+        st.push(arr[i]);
+         
     }
+
+    return age;
+}
 
 
     public static void main(String[] args){
         int[] arr={2,5,9,3,1,12,6,8,7};
-        Next(arr);
+        // Next(arr);
 
     }
 }
