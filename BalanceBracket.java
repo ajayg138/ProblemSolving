72f8cbfcaea4e63b3e1abe7d00d4e5e79bc7e850
ajayg138 <ajaygole728@gmail.com>
2024-11-07 15:48:02 +0530

    BalanceBracket..

--- BalanceBracket.java
+++ BalanceBracket.java
@@ -38,6 +38,38 @@ public class BalanceBracket{
 
     //Balance Bracket....
 
+    public static boolean balanceBrac(String str){
+
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }else if(ch==')'){
+                handleClosing(st,'(');
+            }else if(ch=='}'){
+                handleClosing(st,'{');
+            }else if(ch==']'){
+                handleClosing(st,'[');
+            }
+        }
+    }
+
+
+    public static boolean handleClosing(Stack<Character> st, char corresoch){
+        if(st.size()==0){
+            return false;
+        }else if(st.peek() != corresoch){
+            return false;
+        }else{
+            st.pop();
+            return true;
+        }
+    }
+
+
+
     public static void main(String[] args){
         String str="[(a+b)+{(c+d)*(e/f)}]";
         boolean res=balanceBrac(str);
