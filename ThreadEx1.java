cd3e31e25bbe6fbe11014d9b9a5d60c11a0161ea
ajayg138 <ajaygole728@gmail.com>
2024-11-08 23:38:16 +0530

    SingleThread

--- ThreadEx1.java
+++ ThreadEx1.java
@@ -1,13 +1,16 @@
 import java.lang.*;
 
-
+//Single task from single thread
 public class ThreadEx1 extends Thread{
     public void run(){
-        System.out.print("Single Thread");
+        System.out.println("Single Thread");
     }
 
     public static void main(String[] args){
         ThreadEx1 t=new ThreadEx1();
         t.start();
+        ThreadEx1 t1=new ThreadEx1();
+        t1.start();
+
     }
 }
