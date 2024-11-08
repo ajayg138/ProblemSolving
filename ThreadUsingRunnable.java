97b0593f8792a54ee8d1cabdbed326edc47d230d
ajayg138 <ajaygole728@gmail.com>
2024-11-08 23:19:40 +0530

    ThreadUsingRunnable

--- ThreadUsingRunnable.java
+++ ThreadUsingRunnable.java
@@ -3,7 +3,7 @@ import java.lang.*;
 public class ThreadUsingRunnable implements Runnable{
 
     public void run(){
-
+        System.out.print("Runnable Interface");
     }
 
     public static void main(String[] args){
