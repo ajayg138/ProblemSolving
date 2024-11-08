6b434d6572e9faafdfe0ab7eb0dbba40221874c1
ajayg138 <ajaygole728@gmail.com>
2024-11-09 01:39:24 +0530

    ThreadMethods changes...

--- ThreadMethods.java
+++ ThreadMethods.java
@@ -1,17 +1,32 @@
 import java.util.*;
 import java.lang.*;
 
-public class ThreadMethods{
+public class ThreadMethods extends Thread{
+
+    public void run(){
+        System.out.println("Thread Task: "+Thread.currentThread().getName());
+    }
 
 
     public static void main(String[] args){
 
-        System.out.println("Hello");
+        System.out.println("Hello: "+Thread.currentThread().getName());
+
+        ThreadMethods t=new ThreadMethods();
+        t.start();
         
-        System.out.println(Thread.currentThread().getName());
-        Thread.currentThread().setName("Ajay");
-        System.out.println(Thread.currentThread().getName());
-        System.out.println(10/0);
+        ThreadMethods t1=new ThreadMethods();
+        t1.start();
+
+
+
+
+        // System.out.println("Hello");
+        
+        // System.out.println(Thread.currentThread().getName());
+        // Thread.currentThread().setName("Ajay");
+        // System.out.println(Thread.currentThread().getName());
+        // System.out.println(10/0);
 
     }
 }
