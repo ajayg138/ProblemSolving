c3ca408581e0d98edbbc5d782e077b387e5476c5
ajayg138 <ajaygole728@gmail.com>
2024-11-08 22:43:50 +0530

    ThreadEx

---
+++ ThreadEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ThreadEx extends Thread{
+
+    public void run(){
+
+    }
+
+
+    public static void main(String[] args){
+        ThreadEx t=new ThreadEx();
+        t.start();
+
+
+    }
+}
