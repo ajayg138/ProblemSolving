f95b55ecc08b1d2022a00207bc8b610de889d933
ajayg138 <ajaygole728@gmail.com>
2024-11-09 01:21:46 +0530

    ThreadMethods

---
+++ ThreadMethods.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+
+public class ThreadMethods{
+
+
+    public static void Main(String[] args){
+
+        System.out.print("Hello");
+        System.out.print(Thread.currentThread().getName());
+    }
+}
