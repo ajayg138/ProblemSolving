b84ff42ef0b907d7f14521fd878a19461a2b0bef
ajayg138 <ajaygole728@gmail.com>
2024-11-10 21:22:32 +0530

    ReverseNumber...

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,18 +1,22 @@
 import java.util.*;
 
 public class ReverseNumber{
+//firstWay
+    // public static int reverse(int num){
+    //     int rev=0;
 
-    public static int reverse(int num){
-        int rev=0;
+    //     while(num != 0){
+    //         int ld=num%10;
+    //         rev=(rev*10)+ld;
+    //         num=num/10;
+    //     }
 
-        while(num != 0){
-            int ld=num%10;
-            rev=(rev*10)+ld;
-            num=num/10;
-        }
+    //     return rev;
+    // }
 
-        return rev;
-    }
+//secondway..
+
+public static 
 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
