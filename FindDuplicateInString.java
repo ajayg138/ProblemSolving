8f420e01ed769b133677f22df439a4c582e2b478
ajayg138 <ajaygole728@gmail.com>
2024-11-11 21:25:20 +0530

    FindDuplicateInString...

--- FindDuplicateInString.java
+++ FindDuplicateInString.java
@@ -1,6 +1,32 @@
-public class FindDuplicateInStringFindDuplicateInString{
+import java.util.*;
+
+public class FindDuplicateInString{
+
+public static void findDuplicateCharacters(String str) {
+    List<Character> countedChars = new ArrayList<>();
+
+    for(int i=0;i<str.length();i++){
+        char ch=str.charAt(i);
+        int count=0;
+    
+
+    if(!countedChars.contains(ch)){
+        for(int j=0;j<str.length();j++){
+            if(str.charAt(j) == ch){
+                count++;
+            }
+        }
+
+        if(count>1){
+            System.out.println("Character: "+ch+", count: "+count);
+        }
+        countedChars.add(ch);
+    }
+    }
+}
 
     public static void main(String[] args){
-        String str="";
+        String str = "FindDuplicateInString";
+        findDuplicateCharacters(str);
     }
 }
