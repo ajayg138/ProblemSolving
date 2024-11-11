5b63ee3ce15a5434fd1826071d0cbf288cf41170
ajayg138 <ajaygole728@gmail.com>
2024-11-11 20:33:31 +0530

    MaxSubArrray changes...

--- MaxSubArrray.java
+++ MaxSubArrray.java
@@ -17,6 +17,21 @@ public class MaxSubArrray{
     // }
 
 
+//optimal solution using kadane's algorithm
+
+public static in maxSubarr(int[] arr){
+    int maxSum=arr[o];
+    int currentSum=arr[0];
+
+    for(int i=1;i<arr.length;i++){
+        currentSum=Math.max(arr[i],currentSum+arr[i]);
+        maxSum=Math.max(currentSum,maxSum);
+    }
+
+    return maxSum;
+
+}
+
     public static void main(String[] args){
         int[] arr={-2,1,-3,4,-1,2,1,-5,4};
         int res=maxSubarr(arr);
