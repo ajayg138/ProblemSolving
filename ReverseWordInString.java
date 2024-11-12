4e385b526ae2c8613b49da2a7af997a8017f6079
ajayg138 <ajaygole728@gmail.com>
2024-11-12 23:01:01 +0530

    ReverseWordInString...

--- ReverseWordInString.java
+++ ReverseWordInString.java
@@ -6,23 +6,38 @@ public class ReverseWordInString{
 
     public static String reverseWords(String str){
         // String[] strArray=str.trim().split(" ");
-        String[] strArray=str.trim().split("\\s+");
+        // String[] strArray=str.trim().split("\\s+");
 
-        StringBuilder sb=new StringBuilder();
-        for(int i=strArray.length-1;i>=0;i--){
-            sb.append(strArray[i]);
-            if(i!=0){
-                sb.append(" ");
+        // StringBuilder sb=new StringBuilder();
+        // for(int i=strArray.length-1;i>=0;i--){
+        //     sb.append(strArray[i]);
+        //     if(i!=0){
+        //         sb.append(" ");
+        //     }
+        // }
+
+        // return sb.toString();
+
+
+
+        List<String> words=new ArrayList<>();
+        String[] parts=str.split("\\.");
+
+        for(String word : parts){
+            if(!word.isEmpty()){
+                words.add(word);
             }
         }
 
-        return sb.toString();
+        Collections.reverse(words);
+
+        return String.join(".",words);
 
     }
 
 
     public static void main(String[] args){
-        String str=" Ajay Ramchandra    Gole     ";
+        String str=" Ajay   Ramchandra    Gole     ";
         String res=reverseWords(str);
         System.out.println(res);
 
