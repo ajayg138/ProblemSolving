d4a966179715b6afeab990b28ec22cbb1f5ae645
ajayg138 <ajaygole728@gmail.com>
2024-11-12 20:28:50 +0530

    ReverseWordInString...

--- ReverseWordInString.java
+++ ReverseWordInString.java
@@ -3,11 +3,10 @@ import java.util.*;
 public class ReverseWordInString{
 
     public static String reverseWords(String str){
-        char[] strArray=str.toCharArray().split(" ");
+        String[] strArray=str.split();
 
-        int lastChar=strArray.length-1;
         StringBuilder sb=new StringBuilder();
-        for(int i=lastChar;i>=0;i--){
+        for(int i=strArray.length-1;i>=0;i--){
             sb.append(strArray[i]);
             if(i!=0){
                 sb.append(" ");
