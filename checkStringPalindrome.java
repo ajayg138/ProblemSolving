3fd650dee24e47a0cf60a9faaee6a148d85dab83
ajayg138 <ajaygole728@gmail.com>
2024-11-13 23:51:03 +0530

    checkStringPalindrome... changes...

--- checkStringPalindrome.java
+++ checkStringPalindrome.java
@@ -1,7 +1,24 @@
 public class checkStringPalindrome{
 
+    public static void checkPalindrome(String str){
+        String ogStr = str;
+        StringBuilder rev=new StringBuilder();
+
+        for(int i=str.length()-1;i>=0;i--){
+            rev.append(str.charAt(i));
+        }
+
+        if(ogStr.equals(rev.toString())){
+            System.out.println("String is Palindrome...");
+        }else{
+            System.out.println("String is NOT Palindrome...");
+        }
+    }
+
 
     public static void main(String[] args){
-        String str="ajay";
+        // String str="ajay";
+        String str="madam";
+        checkPalindrome(str);
     }
 }
