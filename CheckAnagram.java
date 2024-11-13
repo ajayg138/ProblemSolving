e8fce44ea1d81a93388ab1fa6517c5c4831e39ff
ajayg138 <ajaygole728@gmail.com>
2024-11-13 23:37:58 +0530

    CheckAnagram...

--- CheckAnagram.java
+++ CheckAnagram.java
@@ -2,6 +2,25 @@ import java.util.*;
 
 public class CheckAnagram{
 
+// Given two strings s and t, return true if t is an 
+// anagram
+//  of s, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+
     public static boolean validateAnagram(String str1, String str2){
         if(str1.length() != str2.length()){
             return false;
