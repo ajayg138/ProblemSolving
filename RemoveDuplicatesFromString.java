97e1d6a13d3e882d219243b63a0dae3bd5f06426
ajayg138 <ajaygole728@gmail.com>
2024-11-14 01:12:13 +0530

    RemoveDuplicatesFromString...

--- RemoveDuplicatesFromString.java
+++ RemoveDuplicatesFromString.java
@@ -22,8 +22,24 @@ public class RemoveDuplicatesFromString{
 //     }
 
 
+public static String RemoveDup(String str){
+
+    HashSet<Character> hs=new HashSet<>();
+
+    StringBuilder sb=new StringBuilder();
+
+    for(int i=0;i<str.length();i++){
+        if(hs.add(str.charAt(i))){
+            sb.append(str.charAt(i));
+        }
+    }
+
+    return sb.toString();
+}
+
+
     public static void main(String[] args){
-        String str="aabbcc";
+        String str="aabbccccc";
 
         String res=RemoveDup(str);
         System.out.println(res);
