8bc415999012f7e4eb42aa74d678f484a628822f
ajayg138 <ajaygole728@gmail.com>
2024-11-13 23:56:27 +0530

    ShortestDistanceBetweenString...

--- ShortestDistanceBetweenString.java
+++ ShortestDistanceBetweenString.java
@@ -1,36 +1,36 @@
-public class ShortestDistanceBetweenString{
+    public class ShortestDistanceBetweenString{
+//ShortestDistanceBetweenString
+        public static int ShortestDist(String[] s,String word1, String word2){
+            int ans=Integer.MAX_VALUE;
 
-    public static int ShortestDist(String[] s,String word1, String word2){
-        int ans=Integer.MAX_VALUE;
+            int index1=-1;
+            int index2=-1;
 
-        int index1=-1;
-        int index2=-1;
+            for(int i=0;i<s.length;i++){
+                if(s[i].equals(word1)){
+                    index1=i;
+                }
 
-        for(int i=0;i<s.length;i++){
-            if(s[i].equals(word1)){
-                index1=i;
+                if(s[i].equals(word2)){
+                    index2=i;
+                }
+
+                if(index1!=-1 && index2!=-1){
+                    ans=Math.min(ans,Math.abs(index1-index2));
+                }
             }
 
-            if(s[i].equals(word2)){
-                index2=i;
-            }
 
-            if(index1!=-1 && index2!=-1){
-                ans=Math.min(ans,Math.abs(index1-index2));
-            }
+            return ans;
         }
 
 
-        return ans;
+        public static void main(String[] args){
+            String[] S = { "the", "quick", "brown", "fox", "quick" };
+    
+            String word1 = "the", word2 = "fox";
+
+            int res=ShortestDist(S,word1,word2);
+            System.out.println(res);
+        }
     }
-
-
-    public static void main(String[] args){
-        String[] S = { "the", "quick", "brown", "fox", "quick" };
- 
-        String word1 = "the", word2 = "fox";
-
-        int res=ShortestDist(S,word1,word2);
-        System.out.println(res);
-    }
-}
