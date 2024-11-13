e518689b4ea2a56226b0a8643dac75bacfc29b98
ajayg138 <ajaygole728@gmail.com>
2024-11-13 23:37:20 +0530

    CheckAnagram...

---
+++ CheckAnagram.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class CheckAnagram{
+
+    public static boolean validateAnagram(String str1, String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+
+        char[] str1Array=str1.toCharArray();
+        char[] str2Array=str2.toCharArray();
+
+        Arrays.sort(str1Array);
+        Arrays.sort(str2Array);
+
+
+        String s1=new String(str1Array);
+        String s2=new String(str2Array);
+
+        if(s1.equals(s2)){
+            return true;
+        }        
+    
+    return false;
+
+    }
+
+    public static void main(String[] args){
+        String str1="rat";
+        String str2="car";
+
+        boolean res=validateAnagram(str1,str2);
+        if(res==true){
+            System.out.println("Anagram...");
+        }else{
+            System.out.println("Not a Anagram...");
+        }
+    }
+}
