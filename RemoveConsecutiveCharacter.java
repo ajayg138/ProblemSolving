9e6b8178a99b5a8d8d06a1c31131b6a0f064bd1d
ajayg138 <ajaygole728@gmail.com>
2024-11-14 18:44:33 +0530

    RemoveConsecutiveCharacter...Changes...

--- RemoveConsecutiveCharacter.java
+++ RemoveConsecutiveCharacter.java
@@ -3,21 +3,60 @@ import java.util.*;
 public class RemoveConsecutiveCharacter{
 
 //Remove Duplicate From String...
-    public static String remove(String str){
-        HashSet<Character> hs=new HashSet<>();
+    // public static String remove(String str){
+    //     HashSet<Character> hs=new HashSet<>();
 
-        StringBuilder sb=new StringBuilder();
+    //     StringBuilder sb=new StringBuilder();
 
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(hs.add(ch)){
-                sb.append(ch);
-            }
+    //     for(int i=0;i<str.length();i++){
+    //         char ch=str.charAt(i);
+    //         if(hs.add(ch)){
+    //             sb.append(ch);
+    //         }
+    //     }
+
+    //     return sb.toString();
+    // }
+
+//Brute Force Approach...O(N)...
+// public static String remove(String str){
+//     String res="";
+
+//     for(int i=0;i<str.length();i++){
+//         if(i==0 || str.charAt(i) != str.charAt(i-1)){
+//             res = res +str.charAt(i);
+//         }
+//     }
+
+//     return res;
+// }
+
+
+// public static String remove(String str){
+//     StringBuilder sb=new StringBuilder();
+
+//     for(int i=0;i<str.length();i++){
+//         if(i==0 || str.charAt(i) != str.charAt(i-1)){
+//             sb.append(str.charAt(i));
+//         }
+//     }
+
+//     return sb.toString();
+// }
+
+//Optimal Approah using StringBuilder...
+
+public static String remove(String str){
+    StringBuilder sb=new StringBuilder();
+
+    for(int i=0;i<str.length();i++){
+        if(i==0 || str.charAt(i) != str.charAt(i-1)){
+            sb.append(str.charAt(i));
         }
-
-        return sb.toString();
     }
 
+    return sb.toString();
+}
 
     public static void main(String[] args){
         String str="aabaa";
