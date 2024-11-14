95e01d986041818c01f52f5c94fc5d61c9cc648d
ajayg138 <ajaygole728@gmail.com>
2024-11-14 14:27:59 +0530

    StringRotation...

--- StringRotation.java
+++ StringRotation.java
@@ -1,11 +1,26 @@
 public class StringRotation{
 
+    public static void checkRotation(String str1,String str2){
+        if(str1.length() != str2.length()){
+            System.out.println("Not a Rotation");
+            System.exit(0);
+        }
 
-    
+        String str3=str1+str1;
+
+        if(str3.indexOf(str2) != -1){
+            System.out.println("Rotation...");
+        }else{
+            System.out.println("Rotation...");
+        }
+    }
+
+
 
     public static void main(String[] args){
         String str1="ABCD";
-        String str2="CDBA";
+        String str2="CDBAA";
+        checkRotation(str1,str2);
 
     }
 }
