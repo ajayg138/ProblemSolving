3173935b9a18e37cfd632ac00c48cec879ead984
ajayg138 <ajaygole728@gmail.com>
2024-11-14 16:57:25 +0530

    checkvalidParenthesis...

---
+++ checkvalidParenthesis.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class checkvalidParenthesis{
+
+    public static boolean isValid(String str){
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }else{
+                if(st.isEmpty() || !matches(st.pop(),ch)){
+                    return false;
+                }
+            }
+        }
+    
+    return st.isEmpty();
+    }
+
+    public static boolean matches(char open, char close){
+        return (open=='(' && close==')') || (open=='[' && close==']') || (open=='{' && close=='}');
+    }
+
+    public static void main(String[] args){
+        String str="[](){{{{{}}}}}";
+
+        boolean res = isValid(str);
+        if(res==true){
+            System.out.println("Valid Parenthesis...");
+        }else{
+            System.out.println("Invalid Parenthesis...");
+        }
+
+    }
+}
