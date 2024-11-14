70608112ebcffdbba4d27655b04a9705969b0fa6
ajayg138 <ajaygole728@gmail.com>
2024-11-14 15:40:22 +0530

    RotateStringByTwo...

--- RotateStringByTwo.java
+++ RotateStringByTwo.java
@@ -2,6 +2,28 @@ public class RotateStringByTwo{
 
 
 //Naive Approach...O(n)
+    // public static boolean isRotated(String str1,String str2){
+    //     if(str1.length() != str2.length()){
+    //         return false;
+    //     }
+
+    //     if(str1.length()<=2 || str2.length()<=2){
+    //         return str1.equals(str2);
+    //     }
+
+    //     String clockRot="";
+    //     String anticlockRot="";
+    //     int len=str2.length();
+
+
+    //     anticlockRot = str2.substring(len-2)+str2.substring(0,len-2);
+
+    //     clockRot = str2.substring(2) + str2.substring(0,2);
+
+    //     return str1.equals(anticlockRot) || str1.equals(clockRot);        
+    // }
+
+
     public static boolean isRotated(String str1,String str2){
         if(str1.length() != str2.length()){
             return false;
@@ -11,16 +33,17 @@ public class RotateStringByTwo{
             return str1.equals(str2);
         }
 
-        String clockRot="";
-        String anticlockRot="";
+        String rotClock="";
+        String rotAntiClock="";
         int len=str2.length();
 
+        rotClock=str2.substring(2) + str2.substring(0,2);
 
-        anticlockRot = str2.substring(len-2)+str2.substring(0,len-2);
+        rotAntiClock=str2.substring(len-2)+str2.substring(0,len-2);
 
-        clockRot = str2.substring(2) + str2.substring(0,2);
+        return str1.equals(rotAntiClock) || str2.equals(rotClock);
 
-        return str1.equals(anticlockRot) || str1.equals(clockRot);        
+
     }
 
     public static void main(String[] args){
