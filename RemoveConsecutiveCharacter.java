06eb09e46434b3aee0f6cff59e067819cd09abb7
ajayg138 <ajaygole728@gmail.com>
2024-11-14 18:30:54 +0530

    RemoveConsecutiveCharacter...

---
+++ RemoveConsecutiveCharacter.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class RemoveConsecutiveCharacter{
+
+//Remove Duplicate From String...
+    public static String remove(String str){
+        HashSet<Character> hs=new HashSet<>();
+
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(hs.add(ch)){
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args){
+        String str="aabaa";
+        // String str="aabb";
+        System.out.println(remove(str));
+    }
+}
