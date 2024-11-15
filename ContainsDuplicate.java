b06a00cf4dc7209d6c47e2bea1dff4bd4e782404
ajayg138 <ajaygole728@gmail.com>
2024-11-15 20:58:20 +0530

    ContainsDuplicate...

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class ContainsDuplicate{
 
 
-//Brute Foce Approach...
-    public static boolean checkDup(int[] arr){
-        Arrays.sort(arr);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]==arr[i-1]){
-                return true;
-            }
-        }
+// //Brute Foce Approach...
+//     public static boolean checkDup(int[] arr){
+//         Arrays.sort(arr);
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]==arr[i-1]){
+//                 return true;
+//             }
+//         }
 
-        return false;
-    }
+//         return false;
+//     }
 
 
 
