1c07b5e746341a3006f6576d1b2523897e2243b0
ajayg138 <ajaygole728@gmail.com>
2024-11-15 20:14:45 +0530

    MinNMaxEleInArray...

--- MinNMaxEleInArray.java
+++ MinNMaxEleInArray.java
@@ -1,24 +1,24 @@
 public class MinNMaxEleInArray{
 
     //First BRute Force...
-    public static void MinMax(int[] arr){
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<min){
-                min=arr[i];
-            }
-        }
-        System.out.println(min);
+    // public static void MinMax(int[] arr){
+    //     int min=Integer.MAX_VALUE;
+    //     for(int i=0;i<arr.length;i++){
+    //         if(arr[i]<min){
+    //             min=arr[i];
+    //         }
+    //     }
+    //     System.out.println(min);
 
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
-            }
-        }
-        System.out.println(max);
+    //     int max=Integer.MIN_VALUE;
+    //     for(int i=0;i<arr.length;i++){
+    //         if(arr[i]>max){
+    //             max=arr[i];
+    //         }
+    //     }
+    //     System.out.println(max);
 
-    }
+    // }
 
 
     public static void main(String[] args){
