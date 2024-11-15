fd384717ee9564d0c6119ba935f3ba0f99c9a8b3
ajayg138 <ajaygole728@gmail.com>
2024-11-15 20:40:41 +0530

    ReverseArray...

--- ReverseArrayprac.java
+++ ReverseArrayprac.java
@@ -1,18 +1,37 @@
+import java.util.*;
+
 public class ReverseArrayprac{
 
 
 //using for loop..O(n);
+    // public static void reverse(int[] arr){
+    //     int n=arr.length;
+    //     int[] rev=new int[arr.length];
+
+    //     for(int i=0;i<n;i++){
+    //         rev[i]=arr[n-1-i];
+    //     }
+
+    //     for(int i:rev){
+    //         System.out.print(i+" ");
+    //     }
+    // }
+
+
     public static void reverse(int[] arr){
         int n=arr.length;
-        int[] rev=new int[arr.length];
+
+        int[] rev=new int[n];
 
         for(int i=0;i<n;i++){
-            rev[i]=arr[n-1-i];
+            rev[i]=arr[n-i-1];
         }
 
-        for(int i:rev){
-            System.out.print(i+" ");
-        }
+        // for(int i:rev){
+        //     System.out.print(i+" ");
+        // }
+
+        System.out.print(Arrays.toString(rev));
     }
 
     public static void main(String[] arg){
