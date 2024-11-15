0fc11a4b0615f1d1ed4e531fbcd66cf63e479fdd
ajayg138 <ajaygole728@gmail.com>
2024-11-15 20:10:19 +0530

    MinNMaxEleInArray...

---
+++ MinNMaxEleInArray.java
@@ -0,0 +1,11 @@
+public class MinNMaxEleInArray{
+
+    public static void MinMax(int[] arr){
+        
+    }
+
+
+    public static void main(String[] args){
+        int[] arr={3, 5, 4, 1, 9};
+    }
+}
